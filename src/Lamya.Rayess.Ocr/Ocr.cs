2bcdbcc5f0fc3483c09773f20e08c5c1aa32e661
lamya-rey <lamyaray97@gmail.com>
2022-03-21 10:13:33 +0100

    ✨ Ocr project

---
+++ src/Lamya.Rayess.Ocr/Ocr.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace Lamya.Rayess.Ocr
+{
+    public class Ocr
+    {
+        public async Task<List<OcrResult>> ReadAsync(List<byte[]> images)
+        {
+            var results = new List<OcrResult>();
+            foreach (var image in images)
+            {
+                await Task.Run(() =>
+                {
+                    // Code
+                    var executingAssemblyPath = Assembly.GetExecutingAssembly().Location;
+                    var executingPath = Path.GetDirectoryName(executingAssemblyPath);
+                    using var engine = new TesseractEngine(Path.Combine(executingPath,
+                        @"tessdata"), "fra", EngineMode.Default);
+                    using var pix = Pix.LoadFromMemory(image);
+                    var test = engine.Process(pix);
+                    var Text = test.GetText();
+                    var Confidence = test.GetMeanConfidence();
+                    results.Add(new OcrResult {Text = Text, Confidence = Confidence});
+                });
+            }
+
+            return results;
+        }
+    }
+}
---
+++ src/Lamya.Rayess.Ocr/OcrResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Lamya.Rayess.Ocr
+{
+    public class OcrResult
+    {
+        public string Text { get; set; }
+        public float Confidence { get; set; } 
+    }
+}
