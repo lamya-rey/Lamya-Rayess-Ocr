0c21ef82b0ba1af3c5f3ad3fe8a9fdd0288c00e2
lamya-rey <lamyaray97@gmail.com>
2022-03-21 10:16:45 +0100

    🔨 Console

---
+++ src/Lamya.Rayess.Ocr.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Lamya.Rayess.Ocr;
+
+var imagesPath = args;
+var images = new List<byte[]>();
+
+foreach (var imagePath in imagesPath)
+{
+    var imageBytes = await File.ReadAllBytesAsync(imagePath);
+    images.Add(imageBytes);
+}
+
+var ocrResults = await new Ocr().ReadAsync(images);
+
+foreach (var ocrResult in ocrResults)
+{
+    Console.WriteLine($"Confidence :{ocrResult.Confidence}");
+    Console.WriteLine($"Text :{ocrResult.Text}");
+}
